8c447fe5c27fe67c26edc6d624aae3149a85c492
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-04-10 09:47:49 +0600

    React router Setup Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 
 function App() {
     return (
-        <div className="App">
+        <>
             <Header />
-        </div>
+            <Outlet />
+        </>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App";
+import "./index.css";
+import Home from "./components/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    // <React.StrictMode>
+    <RouterProvider router={router} />
+    // </React.StrictMode>
+);
