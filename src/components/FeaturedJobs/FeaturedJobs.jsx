24c0839fd23a32de3bde9c165dc8087504d11c9c
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-04-12 00:02:03 +0600

    See all button handled

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,12 +3,17 @@ import FeaturedJobsCard from "../FeaturedJobsCard/FeaturedJobsCard";
 
 const FeaturedJobs = () => {
     const [featuredJobs, setFeaturedJobs] = useState([]);
+    const [showAll, setShowAll] = useState(false);
 
     useEffect(() => {
         fetch("featuredJobs.json")
             .then((res) => res.json())
             .then((data) => setFeaturedJobs([...featuredJobs, ...data]));
     }, []);
+
+    const handleShowAllClick = () => {
+        setShowAll(!showAll);
+    };
 
     return (
         <div className="mt-32 mb-8">
@@ -18,12 +23,21 @@ const FeaturedJobs = () => {
                 you need. Its your future
             </p>
             <div className="mt-8 grid md:grid-cols-2 gap-6 justify-center">
-                {featuredJobs.map((fJob) => (
-                    <FeaturedJobsCard key={fJob.id} fJob={fJob} />
-                ))}
+                {showAll
+                    ? featuredJobs.map((fJob) => (
+                          <FeaturedJobsCard key={fJob.id} fJob={fJob} />
+                      ))
+                    : featuredJobs
+                          .slice(0, 4)
+                          .map((fJob) => (
+                              <FeaturedJobsCard key={fJob.id} fJob={fJob} />
+                          ))}
             </div>
-            <button className="btn btn-info mt-10 text-white">
-                See All Jobs
+            <button
+                onClick={handleShowAllClick}
+                className="btn btn-info mt-10 text-white"
+            >
+                {showAll ? "Show Less Jobs" : "See All Jobs"}
             </button>
         </div>
     );
