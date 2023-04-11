b43f0ed025655bc97aebe3bbe5fcef4bdc701a35
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-04-11 16:53:27 +0600

    Added the blog section

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,20 +1,55 @@
 import React from "react";
+
 import { getBookmarks } from "../../utils/fakeDB";
 import AppliedJobsCard from "../AppliedJobsCard/AppliedJobsCard";
 
 const AppliedJobs = () => {
     const appliedJobs = getBookmarks();
-    console.log(appliedJobs);
+
+    const handleFilter = (id) => {
+        console.log(id);
+    };
+
     return (
         <div className="mt-8">
             <section className="h-[280px] bg-gray-400 flex justify-center items-center">
                 <h1>Applied Jobs</h1>
             </section>
-
             <section className="float-right mt-5">
-                <button className="btn btn-primary">Filter</button>
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn m-1">
+                        Filter By
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+                            />
+                        </svg>
+                    </label>
+                    <ul
+                        tabIndex={0}
+                        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+                    >
+                        <li>
+                            <a onClick={() => handleFilter("remote")}>Remote</a>
+                        </li>
+                        <li>
+                            <a onClick={() => handleFilter("onsite")}>
+                                On Site
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </section>
-
+            ''
             <section className="mt-32">
                 {appliedJobs.map((appliedJob) => (
                     <AppliedJobsCard
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,8 +2,43 @@ import React from "react";
 
 const Blog = () => {
     return (
-        <div>
-            <h1>This is blog</h1>
+        <div className="w-full">
+            <h1 className="my-10 text-center">This is blog</h1>
+            <section className="text-left">
+                <div>
+                    <h2 className="text-3xl">
+                        When should you use context API?
+                    </h2>
+                    <p className="mt-5">
+                        One should use the Context API in React when you need to
+                        pass data from a parent component to multiple levels of
+                        nested child components without manually passing it
+                        through each intermediate component as props.
+                    </p>
+                </div>
+                <div>
+                    <h2 className="text-3xl">What is a custom hook?</h2>
+                    <p className="mt-5">
+                        A custom hook is a reusable function in React that lets
+                        you share logic between components..
+                    </p>
+                </div>
+                <div>
+                    <h2 className="text-3xl">What is useRef?</h2>
+                    <p className="mt-5">
+                        useRef is a React hook that returns a mutable ref object
+                        that can be used to hold a reference to a DOM element or
+                        to persist a value between renders.
+                    </p>
+                </div>
+                <div>
+                    <h2 className="text-3xl">What is useMemo?</h2>
+                    <p className="mt-5">
+                        useMemo is a React hook that memoizes a value and only
+                        recomputes it when its dependencies have changed.
+                    </p>
+                </div>
+            </section>
         </div>
     );
 };
