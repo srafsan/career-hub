b1ed3a8e68d66c2e9c8adf96e7db104b8a41d9c9
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-04-12 19:23:47 +0600

    Navbar Responsive fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header/Header";
 function App() {
     return (
         <div>
-            {/* <Header /> */}
+            <Header />
             <Outlet />
         </div>
     );
