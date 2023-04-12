d936f399549094ab8d44567ab0b8b8998c19acfd
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-04-12 14:30:42 +0600

    Blog responsive done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -5,8 +5,8 @@ const Blog = () => {
         <div className="w-full">
             <h1 className="my-10 text-center">This is blog</h1>
             <section className="text-left">
-                <div>
-                    <h2 className="text-3xl">
+                <div className="sm:mt-10">
+                    <h2 className="font-bold text-lg md:text-3xl sm:text-lg">
                         When should you use context API?
                     </h2>
                     <p className="mt-5">
@@ -16,23 +16,29 @@ const Blog = () => {
                         through each intermediate component as props.
                     </p>
                 </div>
-                <div>
-                    <h2 className="text-3xl">What is a custom hook?</h2>
+                <div className="sm:mt-10">
+                    <h2 className="font-bold text-lg md:text-3xl sm:text-lg">
+                        What is a custom hook?
+                    </h2>
                     <p className="mt-5">
                         A custom hook is a reusable function in React that lets
                         you share logic between components..
                     </p>
                 </div>
-                <div>
-                    <h2 className="text-3xl">What is useRef?</h2>
+                <div className="sm:mt-10">
+                    <h2 className="font-bold text-lg md:text-3xl sm:text-lg">
+                        What is useRef?
+                    </h2>
                     <p className="mt-5">
                         useRef is a React hook that returns a mutable ref object
                         that can be used to hold a reference to a DOM element or
                         to persist a value between renders.
                     </p>
                 </div>
-                <div>
-                    <h2 className="text-3xl">What is useMemo?</h2>
+                <div className="sm:mt-10">
+                    <h2 className="font-bold text-lg md:text-3xl sm:text-lg">
+                        What is useMemo?
+                    </h2>
                     <p className="mt-5">
                         useMemo is a React hook that memoizes a value and only
                         recomputes it when its dependencies have changed.
