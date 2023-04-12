5a42b0bab69c10ea0bd01eea6933873829766076
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-04-12 14:55:50 +0600

    Reponsive for applied jobs done

--- src/components/AppliedJobsCard/AppliedJobsCard.jsx
+++ src/components/AppliedJobsCard/AppliedJobsCard.jsx
@@ -12,20 +12,20 @@ const AppliedJobsCard = ({ appliedJob }) => {
     };
 
     return (
-        <div className="flex justify-between border p-8 mb-6 rounded-lg">
+        <div className="flex flex-col md:flex-row justify-between border p-8 mb-6 rounded-lg">
             <section className="bg-slate-200 rounded-lg px-11 py-24">
-                <img src={logo} alt={name} />
+                <img className="mx-auto" src={logo} alt={name} />
             </section>
             <section className="text-left">
-                <h2 className="text-2xl">{job}</h2>
-                <h2 className="text-xl">{name}</h2>
+                <h2 className="text-5xl md:text-2xl mt-4">{job}</h2>
+                <h2 className="text-3xl md:text-xl mt-4">{name}</h2>
 
-                <div className="my-4 flex">
+                <div className="my-4 flex mt-4">
                     {types.map((type, index) => {
                         return (
                             <p
                                 key={index}
-                                className="border border-blue-400 text-blue-600 text-center w-[91px] mr-4 p-1 rounded-md"
+                                className="border border-blue-400 text-blue-600 text-center w-[150px] md:w-[91px] mr-4 p-1 rounded-md text-3xl md:text-base"
                             >
                                 {type}
                             </p>
@@ -39,11 +39,15 @@ const AppliedJobsCard = ({ appliedJob }) => {
                             src="https://i.ibb.co/Xj7xbv8/Frame-4.png"
                             alt=""
                         />
-                        <p className="text-gray-500">{location}</p>
+                        <p className="text-gray-500 text-3xl md:text-base">
+                            {location}
+                        </p>
                     </div>
                     <div className="flex gap-2">
                         <img src="https://i.ibb.co/g3t0LCT/Frame.png" alt="" />
-                        <p className="text-gray-500">Salary: {salary}</p>
+                        <p className="text-gray-500 text-3xl md:text-base">
+                            Salary: {salary}
+                        </p>
                     </div>
                 </div>
             </section>
@@ -51,7 +55,7 @@ const AppliedJobsCard = ({ appliedJob }) => {
                 <Link to={`/jobs/${id}`}>
                     <button
                         onClick={handleButtonClick}
-                        className="btn btn-info mt-6 text-white"
+                        className="btn btn-info mt-6 text-white text-3xl md:text-base"
                     >
                         Details
                     </button>
