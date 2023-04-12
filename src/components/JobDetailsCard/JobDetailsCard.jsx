368957dbbce44a33e33af942f418f90844d4e01f
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-04-12 18:03:52 +0600

    Job Details reponsive fixed

--- src/components/JobDetailsCard/JobDetailsCard.jsx
+++ src/components/JobDetailsCard/JobDetailsCard.jsx
@@ -20,28 +20,28 @@ const JobDetailsCard = ({ jobs }) => {
     };
 
     return (
-        <div className="sm:flex sm:flex-col">
+        <div className="max-w-screen-lg">
             <div className="grid md:grid-cols-3 gap-4 text-start">
-                <div className="col-span-2 text-gray-500">
-                    <p className="text-4xl md:text-base">
+                <div className="md:col-span-2 text-gray-500">
+                    <p>
                         <strong className="text-black">
                             Job Description:{" "}
                         </strong>{" "}
                         {description}
                     </p>
-                    <p className="mt-6 text-4xl md:text-base">
+                    <p className="mt-6">
                         <strong className="text-black">
                             Job Responsibilities:{" "}
                         </strong>
                         {responsibility}
                     </p>
-                    <p className="mt-6 text-4xl md:text-base">
+                    <p className="mt-6">
                         <strong className="text-black">
                             Educational Requirements: <br /> <br />
                         </strong>
                         {education}
                     </p>
-                    <p className="mt-6 text-4xl md:text-base">
+                    <p className="mt-6">
                         <strong className="text-black">
                             Experience: <br /> <br />
                         </strong>
@@ -49,18 +49,19 @@ const JobDetailsCard = ({ jobs }) => {
                     </p>
                 </div>
 
-                <div className="mt-5 md:mt-0 col-span-1 w-[1040px] md:w-[330px] bg-gray-200 h-full p-7 rounded-lg">
+                <div className="md:col-span-1 bg-gray-200 h-full p-7 rounded-lg">
                     {/* Job Details */}
                     <div>
-                        <h4 className="text-5xl md:text-lg font-bold border-b pb-6 border-gray-400">
+                        <h4 className="text-lg font-bold border-b pb-6 border-gray-400">
                             Job Details
                         </h4>
                         <div className="flex gap-2 mt-6">
                             <img
                                 src="https://i.ibb.co/g3t0LCT/Frame.png"
+                                className="w-6 h-6"
                                 alt="money"
                             />
-                            <p className="text-gray-500 text-3xl md:text-base">
+                            <p className="text-gray-500">
                                 <strong className="text-black">Salary: </strong>
                                 {salary} (Per Month)
                             </p>
@@ -68,9 +69,10 @@ const JobDetailsCard = ({ jobs }) => {
                         <div className="flex gap-2 mt-6">
                             <img
                                 src="https://i.ibb.co/9cy8tZy/Frame-1.png"
+                                className="w-6 h-6"
                                 alt="calender"
                             />
-                            <p className="text-gray-500 text-3xl md:text-sm">
+                            <p className="text-sm text-gray-500">
                                 <strong className="text-black">
                                     Job Title:{" "}
                                 </strong>
@@ -81,15 +83,16 @@ const JobDetailsCard = ({ jobs }) => {
 
                     {/* Contact Information */}
                     <div className="mt-8">
-                        <h4 className="text-5xl md:text-lg font-bold border-b pb-6 border-gray-400">
+                        <h4 className="text-lg font-bold border-b pb-6 border-gray-400">
                             Contact Information
                         </h4>
                         <div className="flex gap-2 mt-6">
                             <img
                                 src="https://i.ibb.co/r0fkq6Q/Frame-2.png"
+                                className="w-6 h-6"
                                 alt="money"
                             />
-                            <p className="text-gray-500 text-3xl md:text-base">
+                            <p className="text-gray-500">
                                 <strong className="text-black">Phone: </strong>
                                 {contact}
                             </p>
@@ -97,9 +100,10 @@ const JobDetailsCard = ({ jobs }) => {
                         <div className="flex gap-2 mt-6">
                             <img
                                 src="https://i.ibb.co/HV4Yc8R/Frame-3.png"
+                                className="w-6 h-6"
                                 alt="calender"
                             />
-                            <p className="text-gray-500 text-3xl md:text-sm">
+                            <p className="text-sm text-gray-500">
                                 <strong className="text-black">Email: </strong>
                                 {contact}
                             </p>
@@ -107,9 +111,10 @@ const JobDetailsCard = ({ jobs }) => {
                         <div className="flex gap-2 mt-6">
                             <img
                                 src="https://i.ibb.co/Xj7xbv8/Frame-4.png"
+                                className="w-6 h-6"
                                 alt="location"
                             />
-                            <p className="text-gray-500 text-3xl md:text-sm">
+                            <p className="text-sm text-gray-500">
                                 <strong className="text-black">
                                     Address:{" "}
                                 </strong>
@@ -120,7 +125,7 @@ const JobDetailsCard = ({ jobs }) => {
                 </div>
             </div>
             <button
-                className="btn btn-info float-right mt-20 md:mt-6 w-full md:w-[330px] text-5xl md:text-base py-5 h-full md:py-0 md:h-0"
+                className="btn btn-info float-right mt-6 w-[330px]"
                 onClick={() => handleAddToDb(id)}
             >
                 Apply Now
