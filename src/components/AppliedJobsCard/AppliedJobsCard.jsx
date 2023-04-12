7d70e8c6b0569cc8232eb3315472d07a471dfdcd
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-04-12 19:12:56 +0600

    Applied jobs responsive fixed

--- src/components/AppliedJobsCard/AppliedJobsCard.jsx
+++ src/components/AppliedJobsCard/AppliedJobsCard.jsx
@@ -17,15 +17,15 @@ const AppliedJobsCard = ({ appliedJob }) => {
                 <img className="mx-auto" src={logo} alt={name} />
             </section>
             <section className="text-left">
-                <h2 className="text-5xl md:text-2xl mt-4">{job}</h2>
-                <h2 className="text-3xl md:text-xl mt-4">{name}</h2>
+                <h2 className="text-2xl">{job}</h2>
+                <h2 className="text-xl">{name}</h2>
 
-                <div className="my-4 flex mt-4">
+                <div className="my-4 flex">
                     {types.map((type, index) => {
                         return (
                             <p
                                 key={index}
-                                className="border border-blue-400 text-blue-600 text-center w-[150px] md:w-[91px] mr-4 p-1 rounded-md text-3xl md:text-base"
+                                className="border border-blue-400 text-blue-600 text-center w-[91px] mr-4 p-1 rounded-md"
                             >
                                 {type}
                             </p>
@@ -33,21 +33,22 @@ const AppliedJobsCard = ({ appliedJob }) => {
                     })}
                 </div>
 
-                <div className="flex gap-6">
+                <div className="flex flex-col md:flex-row gap-6">
                     <div className="flex gap-2">
                         <img
                             src="https://i.ibb.co/Xj7xbv8/Frame-4.png"
+                            className="w-6 h-6"
                             alt=""
                         />
-                        <p className="text-gray-500 text-3xl md:text-base">
-                            {location}
-                        </p>
+                        <p className="text-gray-500">{location}</p>
                     </div>
                     <div className="flex gap-2">
-                        <img src="https://i.ibb.co/g3t0LCT/Frame.png" alt="" />
-                        <p className="text-gray-500 text-3xl md:text-base">
-                            Salary: {salary}
-                        </p>
+                        <img
+                            src="https://i.ibb.co/g3t0LCT/Frame.png"
+                            className="w-6 h-6"
+                            alt=""
+                        />
+                        <p className="text-gray-500">Salary: {salary}</p>
                     </div>
                 </div>
             </section>
@@ -55,7 +56,7 @@ const AppliedJobsCard = ({ appliedJob }) => {
                 <Link to={`/jobs/${id}`}>
                     <button
                         onClick={handleButtonClick}
-                        className="btn btn-info mt-6 text-white text-3xl md:text-base"
+                        className="btn btn-info mt-6 text-white"
                     >
                         Details
                     </button>
